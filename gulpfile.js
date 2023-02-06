164348d3acd0c7eccb18d4a4cc228978f173cb0c
Franco Bertoni <franco.bertoni@alumnos.ucse.edu.ar>
2023-02-06 16:20:08 -0300

    Aligerando imagenes con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,52 @@
-const{ src, dest, watch,parallel} = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 
 //CSS
-const sass = require('gulp-sass')(require('sass'));;
-const plumber= require("gulp-plumber");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 //Imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 
-function css (cb){
-    src("src/scss/**/*.scss") //** */ es para q escuche por todos los archivos
+function css(cb) {
+  src("src/scss/**/*.scss") //** */ es para q escuche por todos los archivos
     .pipe(plumber())
     .pipe(sass())
     .pipe(dest("build/css"));
 
-    cb();
+  cb();
 }
 
-function versionWebp(cb){
+function imagenes(cb) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
 
-    const opciones = {
-        quality:50
-    };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"));
+  cb();
+}
 
-    src("src/img/**/*.{png,jpg}")//{} para varios formatos
+function versionWebp(cb) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}") //{} para varios formatos
     .pipe(webp(opciones))
-    .pipe(dest("build/img"))
-    cb();
+    .pipe(dest("build/img"));
+  cb();
 }
 
-function dev(cb){
-    watch("src/scss/**/*.scss", css)
-    
-    cb();
+function dev(cb) {
+  watch("src/scss/**/*.scss", css);
+
+  cb();
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev =parallel(versionWebp,dev) ;
+exports.dev = parallel(imagenes, versionWebp, dev);
